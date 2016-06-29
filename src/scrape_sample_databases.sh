dec801f6757c165a055aeb38833fadee5e9b60e5
ktt <kttblgh@gmail.com>
2016-06-29 13:44:06 +0200

    add sample sqlite databases using scrapeFB.py extended with reactions

---
+++ src/scrape_sample_databases.sh
@@ -0,0 +1,19 @@
+### Scrape sample databases
+
+# cd to .../FBscraper/src
+
+## Böbe mozgóképei
+# change access token to your own (https://developers.facebook.com/tools/explorer/) before running
+python scrapeFB.py -a EAACEdEose0cBAOv93ZB6G70J8v9icim0bXse3BDNRZB1zzSTt8LaPxzEFhDBn2hKW0TU8H4rqKBP9B4yJDUk5WtSxNiH2fEeb5gF2weoZAYSiguXqGGZAnXwvsJgeRuAcHzy5lnZAaEXbpdSAbeMZA9tas9iHjVy6TTiXmPmfZBWAZDZD -d ../data/bobe.sqlite -i 127167520968693
+
+## Budapest Pride
+# change access token to your own (https://developers.facebook.com/tools/explorer/) before running
+python scrapeFB.py -a EAACEdEose0cBAOv93ZB6G70J8v9icim0bXse3BDNRZB1zzSTt8LaPxzEFhDBn2hKW0TU8H4rqKBP9B4yJDUk5WtSxNiH2fEeb5gF2weoZAYSiguXqGGZAnXwvsJgeRuAcHzy5lnZAaEXbpdSAbeMZA9tas9iHjVy6TTiXmPmfZBWAZDZD -d ../data/budapest_pride.sqlite -i 76748198685
+
+## Budapest Pride 2016
+# change access token to your own (https://developers.facebook.com/tools/explorer/) before running
+python scrapeFB.py -a EAACEdEose0cBAOv93ZB6G70J8v9icim0bXse3BDNRZB1zzSTt8LaPxzEFhDBn2hKW0TU8H4rqKBP9B4yJDUk5WtSxNiH2fEeb5gF2weoZAYSiguXqGGZAnXwvsJgeRuAcHzy5lnZAaEXbpdSAbeMZA9tas9iHjVy6TTiXmPmfZBWAZDZD -d ../data/budapest_pride_2016.sqlite -i 285447861787749
+
+## Tanítanék 2016.03.15.
+# change access token to your own (https://developers.facebook.com/tools/explorer/) before running
+python scrapeFB.py -a EAACEdEose0cBAOv93ZB6G70J8v9icim0bXse3BDNRZB1zzSTt8LaPxzEFhDBn2hKW0TU8H4rqKBP9B4yJDUk5WtSxNiH2fEeb5gF2weoZAYSiguXqGGZAnXwvsJgeRuAcHzy5lnZAaEXbpdSAbeMZA9tas9iHjVy6TTiXmPmfZBWAZDZD -d ../data/tanitanek_20160315.sqlite -i 513643295475511
